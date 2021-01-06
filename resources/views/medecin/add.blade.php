153a47c173335b9ecaea2f5e155a787b3ab62b9c
desbaa32 <76529262+desbaa32@users.noreply.github.com>
2021-01-06 10:49:47 +0000

    gestion medecin 1

--- resources/views/medecin/add.blade.php
+++ resources/views/medecin/add.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.app')
-
+@section('title','Ajout')
 @section('content')
-    page d'ajout
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Ajouter un Medcecin') }}</div>
+                    <div class="card-body">
+                        {{ __(' HELLO !!!: ') }}
+                        <br>
+                        {{ Auth::user()->name }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/medecin/list.blade.php
+++ resources/views/medecin/list.blade.php
@@ -1,1 +1,18 @@
-lister OK
+@extends('layouts.app')
+@section('title','Liste')
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Liste des Medcecins') }}</div>
+                    <div class="card-body">
+                        {{ __(' HELLO !!!: ') }}
+                        <br>
+                        {{ Auth::user()->name }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
